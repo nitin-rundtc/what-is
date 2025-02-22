8f8240b46b50617aac4c13c1e562d40e6068e7f6
nitin <nitin.kanade@rundtc.com>
2025-02-22 17:05:25 +0530

    v1.5

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,12 +1,16 @@
 import Container from "@/app/_components/container";
+import { EXAMPLE_PATH } from "@/lib/constants";
+
+
 
 export function Footer() {
   const currentYear = new Date().getFullYear();
 
+
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+      <div className="py-28 flex flex-col lg:flex-row items-center">
           <p className="text-center text-sm text-neutral-400 dark:text-neutral-300">
             © {currentYear} ni18 Blog. All rights reserved. 
             Content & resources protected under 
@@ -24,3 +28,5 @@ export function Footer() {
     </footer>
   );
 }
+
+export default Footer;
